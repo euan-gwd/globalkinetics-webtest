543c1316087dca4945a056e28caa73078c9fefb7
euan-gwd <euan@amwic.co.za>
2017-10-31 13:46:56 +0200

    Add render tests

--- src/components/CurrentWeather.test.js
+++ src/components/CurrentWeather.test.js
@@ -6,9 +6,14 @@ import ForecastWeather from './ForecastWeather';
 
 configure({ adapter: new Adaptor() });
 
-describe('<CurrentWeather/>', () => {
+describe('Component Render Tests', () => {
   it('should render the ForecastWeather element', () => {
     const wrapper = shallow(<CurrentWeather />);
     expect(wrapper.find(ForecastWeather)).toHaveLength(1);
+  });
+
+  it('should render a div element with the css class of current_weather_body', () => {
+    const wrapper = shallow(<div className="current_weather_body" />);
+    expect(wrapper.find('.current_weather_body').exists()).toBe(true);
   });
 });
